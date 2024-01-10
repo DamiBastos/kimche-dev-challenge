a3dbe3881062a62fcfb83348cbcde283f63f7672
Damián Bastos <damian.bastos@yahoo.com>
2024-01-10 18:10:53 -0300

    arreglado error de estilo

--- challenge-dev/src/components/CharacterModal.jsx
+++ challenge-dev/src/components/CharacterModal.jsx
@@ -2,12 +2,16 @@
 export const CharacterModal = ({ character, onClose }) => (
   <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
     <div className="relative bg-[#42b4ca] m-2 rounded-md border-4 border-[#bfde42] bg-opacity-90 p-4 rounded-md text-black">
-      <button
-        className="absolute top-2 right-2 bg-[#bfde42] hover:bg-gray-300 rounded-full w-6 h-6 flex items-center justify-center"
-        onClick={onClose}
-      >
-        x
-      </button>
+      <div>
+        <button
+          className="absolute rounded-full top-2 right-2 hover:bg-gray-300   "
+          onClick={onClose}
+        >
+          <p className="bg-[#bfde42] hover:bg-gray-300 rounded-full p-3 w-6 h-6 flex items-center justify-center">
+            X
+          </p>
+        </button>
+      </div>
       <h2 className="text-2xl text-center font-bold mb-1">{character.name}</h2>
       <img src={character.image} alt={character.name} />
       <div className="font-bold">
